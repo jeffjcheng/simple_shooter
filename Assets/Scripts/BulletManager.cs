e0292a93d3ea7d847204d74723c1a63844237c77
Jeffrey Cheng <jeffjcheng+git@gmail.com>
2012-01-22 17:03:00 -0800

    switched to cubes (from planes) to draw textures

--- Assets/Scripts/BulletManager.cs
+++ Assets/Scripts/BulletManager.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class BulletManager : MonoBehaviour {
+	public GameObject bulletPrefab;
+	private List<Transform> bullets;
+	private List<Transform> inactive;
 
 	// Use this for initialization
 	void Start () {
-	
+		bullets = new List<Transform>( 50);
+		inactive = new List<Transform>( 50 );
 	}
 	
 	// Update is called once per frame
