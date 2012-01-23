240ddd4fc1eb1963ea708fd4934076ba8a74286d
Jeffrey Cheng <jeffjcheng+git@gmail.com>
2012-01-23 02:10:23 -0800

    added some bullet functionality + test stuffs

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -9,8 +9,26 @@ public class Bullet {
 		transform = t;
 	}
 	
+	public void SetInactive() {
+		transform.gameObject.active = false;
+		velocity = Vector3.zero;
+		transform.position = Vector3.one * 999f;
+	}
+	
+	public void SetActive() {
+		transform.gameObject.active = true;
+	}
+	
 	public void Tick() {
 		transform.position += velocity * Time.deltaTime;
+	}
+	
+	public void SetPosition( Vector3 pos ) {
+		transform.position = pos;
+	}
+	
+	public void AddVelocity( Vector3 velo ) {
+		velocity += velo;
 	}
 	
 	public void SetVelocity( Vector3 velo ) {
--- Assets/Scripts/BulletManager.cs
+++ Assets/Scripts/BulletManager.cs
@@ -2,19 +2,75 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class BulletManager : MonoBehaviour {
-	public GameObject bulletPrefab;
-	private List<Transform> bullets;
-	private List<Transform> inactive;
+namespace UnityEngine {
+	public delegate IEnumerator DelayedAction( params object[] o );
+}
 
-	// Use this for initialization
+public class BulletManager : MonoBehaviour {
+	private static BulletManager ins;
+	public GameObject bulletPrefab;
+	private List<Bullet> bullets;
+	private Queue<Bullet> inactive;
+	private Transform myBullets;
+
 	void Start () {
-		bullets = new List<Transform>( 50);
-		inactive = new List<Transform>( 50 );
+		myBullets = new GameObject( gameObject.name+"Bullets" ).transform;
+		bullets	 = new List<Bullet>( 10 );
+		inactive = new Queue<Bullet>( 25 );
+		
+		ins = this;
+		
+		// start with some bullets
+		for( int a = 0 ; a < 25 ; a++ ) {
+			GameObject go = (GameObject)Instantiate( bulletPrefab );
+			go.transform.parent = myBullets;
+			Bullet b = new Bullet( go.transform );
+			b.SetInactive();
+			
+			inactive.Enqueue( b );
+		}
+		
+		StartCoroutine( first( ) );
 	}
 	
-	// Update is called once per frame
 	void Update () {
+		foreach( Bullet b in bullets ) {
+			b.Tick();
+		}
+	}
 	
+	public static void DoCoroutine( DelayedAction func, params object[] o ) {
+		ins.StartCoroutine( func( o ) );
+	}
+	
+	public static Bullet GetBullet() {
+		if( ins.inactive.Count > 0 ) {
+			Bullet bullet = ins.inactive.Dequeue();
+			bullet.SetActive();
+			ins.bullets.Add( bullet );
+			return bullet;
+		}
+		
+		GameObject go = (GameObject)Instantiate( ins.bulletPrefab );
+		go.transform.parent = ins.myBullets;
+		Bullet b = new Bullet( go.transform );
+		ins.bullets.Add( b );
+		return b;
+	}
+	
+	IEnumerator first() {
+		float rof = 0.5f;
+		while( true ) {
+			yield return new WaitForSeconds( rof );
+			Bullet b = GetBullet();
+			b.SetPosition( transform.position );
+			b.SetVelocity( Vector3.down );
+			DoCoroutine( test, b );
+		}
+	}
+	
+	IEnumerator test( params object[] o ) {
+		yield return new WaitForSeconds( Random.value+0.5f );
+		((Bullet)o[0]).SetVelocity( new Vector3( 0.5f - Random.value, 0f, 0f ) );
 	}
 }
